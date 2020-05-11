0ae8468ea187f2235393998d558ee563629f3ce2
Hemanshu Varma <hemanshu.varma@scientificgames.com>
2020-05-11 12:38:06 +0530

    Add HomeFragment

--- app/src/main/java/com/varma/hemanshu/firebasefunctions/MainActivity.kt
+++ app/src/main/java/com/varma/hemanshu/firebasefunctions/MainActivity.kt
@@ -1,12 +1,17 @@
 package com.varma.hemanshu.firebasefunctions
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.varma.hemanshu.firebasefunctions.ui.HomeFragment
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.container, HomeFragment.newInstance()).commitNow()
+        }
     }
 }
